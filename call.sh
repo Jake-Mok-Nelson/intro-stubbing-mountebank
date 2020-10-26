0ce2a5f2b79e9c65ffb1291f765e3e5226acc015
Jake Mok Nelson <jakesmnelson@gmail.com>
2020-10-26 15:56:02 +1100

    Implement global auth header rule
    Auth header stub requires that a user be authorised to access any API other than /test

--- call.sh
+++ call.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 
-curl -ivk http://localhost:4545/nottest
+# Returns 200 ok
+curl -ivk http://localhost:4545/test
+
+# Requires auth headers, returns 401
+curl -ivk http://localhost:4545/user
